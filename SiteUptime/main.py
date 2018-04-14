e2042590341aeef0453f1c9826455023c5fdd274
Derek.S <derekseli@outlook.com>
2018-04-14 21:48:58 +0800

    增加日志系统

--- SiteUptime/main.py
+++ SiteUptime/main.py
@@ -35,12 +35,15 @@ class Checker:
         elif re.findall(re_string, raw_url) != 1:
             self.url = args.url.split("/")[-1]
 
+        # 开启日志
+        self.log = Dylog()
 
     def Ping_Testing(self):
         """
         Ping测试
         :return: 测试结果，不通为0，通为1
         """
+        self.log.writelog(getDate(), self.url, "开始Ping测试")
         print("##### Ping %s Test #####" % (self.url))
         ping_test = "ping -c 5 -W 3 %s" % (self.url)
         subp = subprocess.Popen(
@@ -53,8 +56,10 @@ class Checker:
         ping_result = str(subp.stdout.read()) # class byte to str
         ping_regex = re.findall("100% packet loss", ping_result)
         if len(ping_regex) == 0:
+            self.log.writelog(getDate(), self.url, "Ping测试正常")
             return(1)
         else:
+            self.log.writelog(getDate(), self.url, "Ping测试失败 100% packet loss")
             return(0)
 
     def Curl_HttpTesting(self):
@@ -63,6 +68,7 @@ class Checker:
         测试连接、网站状态码、DNS解析时间、连接时间等
         :return:
         """
+
         curl = pycurl.Curl()
 
         # 将curl返回页面内容重定向到文件 pycutl.writexxx 为二进制写入，注意文件打开模式
@@ -71,8 +77,8 @@ class Checker:
 
         # 配置pycurl
         curl.setopt(pycurl.URL, self.url)
-        curl.setopt(pycurl.CONNECTTIMEOUT, 3)  # 设置请求连接等待时间为5秒
-        curl.setopt(pycurl.TIMEOUT, 3)  # 设置超时时间为3秒
+        curl.setopt(pycurl.CONNECTTIMEOUT, 5)  # 设置请求连接等待时间为5秒
+        curl.setopt(pycurl.TIMEOUT, 5)  # 设置超时时间为5秒
         curl.setopt(pycurl.NOPROGRESS, 1)  # 关闭下载进度条
         curl.setopt(pycurl.FORBID_REUSE, 1)  # 交互后关闭连接，不重用连接
         curl.setopt(pycurl.MAXREDIRS, 1)  # 重定向最大次数为1次
@@ -80,15 +86,22 @@ class Checker:
 
         # work
         try:
+            self.log.writelog(getDate(), self.url, "开始curl http状态测试")
             curl.perform()
-            nslookup_time = curl.getinfo(curl.NAMELOOKUP_TIME)
-            connect_time = curl.getinfo(curl.CONNECT_TIME)
-            http_code = curl.getinfo(curl.HTTP_CODE)
+            nslookup_time = str(round(curl.getinfo(curl.NAMELOOKUP_TIME), 2))
+            connect_time = str(round(curl.getinfo(curl.CONNECT_TIME), 2))
+            http_code = str(curl.getinfo(curl.HTTP_CODE))
             print(nslookup_time, connect_time, http_code)
+            self.log.writelog(
+                getDate(),
+                self.url,
+                "测试结束 状态码" + http_code + " DNS查询时间 " + nslookup_time + "秒 连接用时 " + connect_time + "秒"
+            )
             content.close()
             curl.close()
         except Exception as e:
             print(e)
+            self.log.writelog(getDate(), self.url, "curl http状态测试失败 错误信息：" + str(e) )
             content.close()
             curl.close()
 
@@ -100,6 +113,23 @@ class weChat:
 
     def send_mes(self,message):
         itchat.send(message, toUserName="filehelper")
+
+
+class Dylog:
+    # 日志系统(简单实现)
+    def __init__(self):
+        filename = str(arrow.now().format("YYYY-MM-DD")) + "_log.txt"
+        self.logfile = open(filename, "a+", encoding="utf-8")
+
+    def writelog(self, logdate, target, logmessage):
+        logdate_str = "[" + str(logdate) + "] - " + target + " - " + logmessage
+        self.logfile.write(logdate_str + "\n")
+
+    def logfile_close(self):
+        self.logfile.close()
+
+def getDate():
+    return str(arrow.now().format("YYYY-MM-DD HH:mm:ss"))
 
 
 if __name__ == "__main__":
