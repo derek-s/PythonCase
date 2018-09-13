596c88b8194f7cdb2dba811bc8e6e12fe9ba1ba7
Derek.S <derekseli@outlook.com>
2018-09-13 11:12:59 +0800

    tesing

--- zabbix_smsbao/main.py
+++ zabbix_smsbao/main.py
@@ -1,4 +1,4 @@
-# #!/usr/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 # File:main.py
 # Project:zabbix_smsbao
@@ -9,19 +9,20 @@
 # -----
 
 import requests
+import sys
 
 
 def sendSms(phoneNumber, content):
     statusStr = {
-        '0': '短信发送成功',
-        '-1': '参数不全',
-        '-2': '服务器空间不支持,请确认支持curl或者fsocket,联系您的空间商解决或者更换空间',
-        '30': '密码错误',
-        '40': '账号不存在',
-        '41': '余额不足',
-        '42': '账户已过期',
-        '43': 'IP地址限制',
-        '50': '内容含有敏感词'
+        '0': '0',
+        '-1': '-1',
+        '-2': '-2',
+        '30': '30',
+        '40': '40',
+        '41': '41',
+        '42': '42',
+        '43': '43',
+        '50': '50'
     }
 
 
@@ -32,9 +33,9 @@ def sendSms(phoneNumber, content):
     params = {"u":user, "p":pwd, "m":phoneNumber, "c":content}
 
     r = requests.get(smsapi, params=params)
-    print(r.text)
-    print(statusStr[r.text])
+    #print(r.text)
+    #print(statusStr[r.text])
 
 
 if __name__ == "__main__":
-    sendSms("手机号码", "短信内容")
+    sendSms(sys.argv[1],sys.argv[2])
